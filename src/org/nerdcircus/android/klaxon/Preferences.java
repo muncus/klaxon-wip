e02b1f128107bb6e756da84f4fa93c9b9ec6fcea
muncus <muncus@c4afc712-db7e-11dd-87b6-9d1a99559f42>
2010-03-08 00:40:10 +0000

    make the 'consume sms' pref be disabled in <1.6
    git-svn-id: https://klaxon.googlecode.com/svn/trunk@47 c4afc712-db7e-11dd-87b6-9d1a99559f42

--- src/org/nerdcircus/android/klaxon/Preferences.java
+++ src/org/nerdcircus/android/klaxon/Preferences.java
@@ -21,6 +21,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.os.Build;
+
+import android.util.Log;
 
 import org.nerdcircus.android.klaxon.ReplyList;
 
@@ -37,6 +40,16 @@ public class Preferences extends PreferenceActivity {
         Intent i = new Intent(Intent.ACTION_MAIN);
         i.setClass(this, ReplyList.class);
         replylist.setIntent(i);
+
+        //disable the "Consume SMS" option if the build is too low
+        //NB: there's no code to act on this, since the abortBroadcast() 
+        // call will not break anything when called in < 1.6
+        Log.d("BUILDVERSION", Build.VERSION.SDK);
+        if(Integer.valueOf(Build.VERSION.SDK) == Integer.valueOf(3)){
+            Preference csp = this.findPreference("consume_sms_message");
+            csp.setEnabled(false);
+        }
+        
     }
 
 }
