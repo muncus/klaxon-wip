c98c45a2a2a3dabe3801d48218f91ed05226a149
muncus <muncus@c4afc712-db7e-11dd-87b6-9d1a99559f42>
2010-04-21 21:49:17 +0000

    unittest scaffolding for pageparser tests.
    git-svn-id: https://klaxon.googlecode.com/svn/trunk@79 c4afc712-db7e-11dd-87b6-9d1a99559f42

---
+++ tests/src/org/nerdcircus/android/klaxon/tests/pageparser/StandardTest.java
@@ -0,0 +1,17 @@
+package org.nerdcircus.android.klaxon.tests.pageparser;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.InstrumentationTestCase;
+import android.os.Bundle;
+import junit.framework.TestCase;
+
+import org.nerdcircus.android.klaxon.Alert;
+import org.nerdcircus.android.klaxon.pageparser.Standard;
+
+public class StandardTest extends TestCase {
+
+    public void testNoop(){
+        assertTrue(true);
+    }
+
+}
