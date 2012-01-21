373f8b3f4953e9a494aaceb355d512d92ee5f977
muncus@gmail.com <muncus@gmail.com@c4afc712-db7e-11dd-87b6-9d1a99559f42>
2012-01-21 19:34:34 +0000

    a bit of monkeyrunner messing
    git-svn-id: https://klaxon.googlecode.com/svn/trunk@140 c4afc712-db7e-11dd-87b6-9d1a99559f42

--- scripts/monkey_tests.py
+++ scripts/monkey_tests.py
@@ -1,6 +1,8 @@
 # monkeyrunner script to install current klaxon, and run its tests.
 # Author: Marc Dougherty <muncus@gmail.com>
 
+import os
+
 from com.android.monkeyrunner import MonkeyDevice, MonkeyRunner
 
 INSTRUMENTATION_RUNNER = 'android.test.InstrumentationTestRunner'
@@ -18,6 +20,12 @@ assert 'OK' in test_dict['stream']
 print test_dict['stream']
 
 # try to send some smses through adb.
+def sendSms(sender="11111", text="blah"):
+  os.system("adb emu 'sms send %(sender)s %(text)s'" % {
+      'sender': sender,
+      'text': text,})
+
+sendSms(text="blah blah blah")
 
 #import socket
 #sock = socket.socket()
